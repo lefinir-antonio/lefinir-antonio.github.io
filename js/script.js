df93fa69235f3c0b075b24852eb4740c956cb066
Tony <antonio.lefinir@gmail.com>
2019-09-27 17:58:56 -0300

    adding array operation topic1

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ function myMessage() {
 function myMessage2() {
 	alert("Test2");
 }
+
+var myarr = [];
+ 
+function addItem() {
+	myarr.push(document.getElementById("name").value);
+	document.getElementById('itemsLength').innerHTML = myarr.length;
+	document.getElementById('items').innerHTML = "[" + myarr.join(" , ") + "]";
+	console.log(myarr); //to confirm it has been added to the array
+}
