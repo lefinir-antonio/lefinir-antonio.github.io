fe9b731a5287b3e83784f52735c625094107e68d
Tony <antonio.lefinir@gmail.com>
2019-10-01 15:38:20 -0300

    adding footer menu

--- js/topic1.js
+++ js/topic1.js
@@ -7,7 +7,7 @@ function addItem() {
 	myarr.push(document.getElementById("name").value);
 	document.getElementById('itemsLength').innerHTML = myarr.length + 1;
 	document.getElementById('items').innerHTML = "[" + myarr.join(" , ") + "]";
-	console.log(myarr); //to confirm it has been added to the array
+	document.getElementById("name").focus();
 }
 
 function infoArray() {
@@ -18,16 +18,13 @@ function infoArray() {
 		if (parseInt(myarr[i]) > max) max = parseInt(myarr[i]);
 		if (parseInt(myarr[i]) < min) min = parseInt(myarr[i]);
 		s=s+parseInt(myarr[i]);
-		//console.log(i, "max", max); 
-		//console.log(i, "min", min); 
 	}
 	console.log(s);
 	console.log(i);
 	var average=s/i;
 
 	if(s!=0) document.getElementById('results').innerHTML = "Results: " + "Max: " + max + " || " + "Min: " + min + " || " + "Average: " + average.toFixed(2);
-	//console.log("final", max); 
-	//console.log("final", min); //to confirm it has been added to the array			
+		
 }
 
 function myGraph() {
@@ -35,11 +32,9 @@ function myGraph() {
 	var c = document.getElementById("myCanvas");
 	var ctx = c.getContext("2d");
 	ctx.beginPath();
-	//ctx.moveTo(0, 150);
 	for (var i = 0; i < myarr.length; i++) {
 		varx=varx+50;
 		ctx.lineTo(varx, 300-parseInt(myarr[i]));
 	}
 	ctx.stroke();
-
 }
