e16acea3163100936c181f02e78787ce546a1ba6
Antonio Lefinir <alefinir@byui.edu>
2019-10-01 12:14:37 -0300

    adding control of inputs

--- js/topic1.js
+++ js/topic1.js
@@ -3,8 +3,9 @@ var myarr = [];
 function addItem() {
 	if (myarr.length >= 12) return;
 	if (!document.getElementById("name").value) return;
+	if (parseInt(document.getElementById("name").value) < 0 || parseInt(document.getElementById("name").value) > 300) return;	
 	myarr.push(document.getElementById("name").value);
-	document.getElementById('itemsLength').innerHTML = myarr.length;
+	document.getElementById('itemsLength').innerHTML = myarr.length + 1;
 	document.getElementById('items').innerHTML = "[" + myarr.join(" , ") + "]";
 	console.log(myarr); //to confirm it has been added to the array
 }
@@ -12,12 +13,19 @@ function addItem() {
 function infoArray() {
 	max=parseInt(myarr[0]);
 	min=parseInt(myarr[0]);
+	s=0;
 	for (var i = 0; i < myarr.length; i++) {
 		if (parseInt(myarr[i]) > max) max = parseInt(myarr[i]);
 		if (parseInt(myarr[i]) < min) min = parseInt(myarr[i]);
+		s=s+parseInt(myarr[i]);
 		//console.log(i, "max", max); 
 		//console.log(i, "min", min); 
 	}
+	console.log(s);
+	console.log(i);
+	var average=s/i;
+
+	if(s!=0) document.getElementById('results').innerHTML = "Results: " + "Max: " + max + " || " + "Min: " + min + " || " + "Average: " + average.toFixed(2);
 	//console.log("final", max); 
 	//console.log("final", min); //to confirm it has been added to the array			
 }
