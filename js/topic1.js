589e45e904bade1da97d965d9bb71b3043261c19
Antonio Lefiñir <antonio.lefinir@gmail.com>
2019-10-01 23:33:53 -0300

    adding comments

--- js/topic1.js
+++ js/topic1.js
@@ -1,28 +1,34 @@
 var myarr = [];
  
 function addItem() {
+	//some checks 
 	if (myarr.length >= 12) return;
 	if (!document.getElementById("name").value) return;
-	if (parseInt(document.getElementById("name").value) < 0 || parseInt(document.getElementById("name").value) > 300) return;	
+	if (parseInt(document.getElementById("name").value) < 0 || parseInt(document.getElementById("name").value) > 300) return;
+	//save value into the array
 	myarr.push(document.getElementById("name").value);
+	//prints result
 	document.getElementById('itemsLength').innerHTML = myarr.length + 1;
 	document.getElementById('items').innerHTML = "[" + myarr.join(" , ") + "]";
+	//set focus over textbox
 	document.getElementById("name").focus();
 }
 
 function infoArray() {
+	//I assume that the first item is the max and the min
 	max=parseInt(myarr[0]);
 	min=parseInt(myarr[0]);
+	//I create an variable to sums items
 	s=0;
+	//I search the max/min values and adds items
 	for (var i = 0; i < myarr.length; i++) {
 		if (parseInt(myarr[i]) > max) max = parseInt(myarr[i]);
 		if (parseInt(myarr[i]) < min) min = parseInt(myarr[i]);
 		s=s+parseInt(myarr[i]);
 	}
-	console.log(s);
-	console.log(i);
+	//calculate the average
 	var average=s/i;
-
+	//check that the array is not empty
 	if(s!=0) document.getElementById('results').innerHTML = "Results: " + "Max: " + max + " || " + "Min: " + min + " || " + "Average: " + average.toFixed(2);
 		
 }
