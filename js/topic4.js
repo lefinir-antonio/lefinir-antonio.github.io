5f59a8468e92c63a847cf276358551c8c5b68728
Tony <antonio.lefinir@gmail.com>
2019-10-10 18:07:27 -0300

    adding maps support

--- js/topic4.js
+++ js/topic4.js
@@ -1,3 +1,23 @@
+function viewCity() { 
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+        //document.getElementById("demo").innerHTML = myArr[0];
+        console.log(myArr[0]);
+        }
+    };
+    xmlhttp.open("GET", "../code_topic4/uruguay.json", true);
+    xmlhttp.send();
+//---------------crear la tabla
+
+ 
+for (i=0; i < myArr.length; i++) {
+    myRow = '<td>' + myArr[i].name() + '</td>' + myArr[i].coord() + '<td></td><td></td><td></td>';
+    document.getElementById("myTable").insertRow(-1).innerHTML = myRow;
+}
+
+}
 function viewWeather() {
     var req = new XMLHttpRequest();
     req.open('GET', 'https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22', true);
@@ -10,16 +30,4 @@ function viewWeather() {
       }
     };
     req.send(null);
-  };
-function viewCity() { 
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        //document.getElementById("demo").innerHTML = myArr[0];
-        console.log(myArr[0]);
-        }
-    };
-    xmlhttp.open("GET", "../code_topic4/uruguay.json", true);
-    xmlhttp.send();
-}
+  }
