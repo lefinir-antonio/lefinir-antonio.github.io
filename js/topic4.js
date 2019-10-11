f2f88a69a876418da378e7751fe6f0497e0af2f7
Tony <antonio.lefinir@gmail.com>
2019-10-11 17:43:06 -0300

    check weather service change url access

--- js/topic4.js
+++ js/topic4.js
@@ -38,7 +38,7 @@ function viewWeather(coutryId) {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        console.log(JSON.parse(this.responseText));
+        console.log(this.responseText);
          wCountry = JSON.parse(this.responseText);
         }
     };
