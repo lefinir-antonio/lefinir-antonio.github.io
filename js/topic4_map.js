749ec8156ed7b4a54c8b1dfe4121bef408abf53b
Tony <antonio.lefinir@gmail.com>
2019-10-14 16:38:09 -0300

    delete maps

--- js/topic4_map.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlZmluaXIiLCJhIjoiY2sxanF5enhxMG51azNtb2QxY2Mwa215YiJ9.vr6zgo6_9GDWqYoL8xh2lg';
-var map = new mapboxgl.Map({
-container: 'map', // container id
-style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-center: [-54.383331, -33.23333], // starting position [lng, lat]
-zoom: 6 // starting zoom
-});
