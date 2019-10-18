d10e8f4d9d8e32fb262ca96323db8a96daac58ad
Tony <antonio.lefinir@gmail.com>
2019-10-18 10:27:54 -0300

    adding storage supporte

--- js/topic4.js
+++ js/topic4.js
@@ -1,5 +1,9 @@
 var wCountry;
+var saveTable;
 var count_loops = 0;
+var db, myObj, myJSON, text, obj, line;
+
+//this function use a json file to provide all cities of uruguay
 function viewCity() {
   if(count_loops !=1){ 
     var xmlhttp = new XMLHttpRequest();
@@ -17,32 +21,14 @@ function viewCity() {
   }
 }
 
-/*
-function viewWeather(coutryId) {
-    var req = new XMLHttpRequest();
-    var testigo = 'https://api.openweathermap.org/data/2.5/weather?id=' + coutryId + "&appid=348f31d3a42d06a5db44f7fa4b9f34a9";
-    console.log(testigo);
-    req.open('GET', 'https://api.openweathermap.org/data/2.5/weather?id=' + coutryId + "&appid=348f31d3a42d06a5db44f7fa4b9f34a9", true);
-    req.onreadystatechange = function (aEvt) {
-      if (req.readyState == 4) {
-         if(req.status == 200)
-          console.log(req.responseText);
-          //return JSON.parse(req.responseText);
-         else
-          console.log("Error loading page\n");
-      }
-    };
-    req.send(null);
-  }
-*/
-//this function shoulbe create the table IMPORTANT
-  function viewWeather(coutryId) { 
+//with this function  I obtain all temperatures and status of every city
+function viewWeather(coutryId) { 
     var xmlhttp = new XMLHttpRequest();
     var myF = 0;
     xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
          wCountry = JSON.parse(this.responseText);
-         myF = ((wCountry.main.temp.toFixed(2)-273.15) * 9/5) + 32;
+         myF = ((wCountry.main.temp.toFixed(2)-273.15) * 9/5) + 32; //convert temperature kelvin to farenheit
          myRow = '<td>' + wCountry.name + '</td><td>' + myF.toFixed(2) + ' F</td><td>'+ wCountry.weather[0].description + '</td>';
          document.getElementById("myTable").insertRow(-1).innerHTML = myRow;
          console.log(wCountry);
@@ -53,3 +39,4 @@ function viewWeather(coutryId) {
     xmlhttp.send();
 
 }
+
