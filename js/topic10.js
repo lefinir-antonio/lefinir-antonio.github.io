55eb7772c1a24f1d8643efc1d4996c427f62c11d
Antonio Lefinir <alefinir@byui.edu>
2019-11-14 23:35:17 -0300

    adding week10

--- js/topic10.js
+++ js/topic10.js
@@ -1,30 +1,83 @@
-function playAudio() { 
-    var image = document.getElementById('myImage');
-    if (image.src.match("2.png")) {
-        image.src = "../img/3.png";
-        image.style.width = '108px';
-        image.style.height = '147px';
-        var x2 = document.getElementById('myAudio2');        
-        x2.play();    
-        document.getElementById('myButton').innerHTML="Back!";
-        return;
-    }
-    if (image.src.match("1.png")) {
-        image.style.width = '189px';
-        image.style.height = '187px';        
-        image.src = "../img/2.png";
-		var x = document.getElementById('myAudio');
-		document.getElementById('myButton').innerHTML="More!";
-        x.play();
-        return;       	             	
-    }
-    if (image.src.match("3.png")) {
-        image.style.width = '131px';
-        image.style.height = '190px';        
-        image.src = "../img/1.png";  	             
-        document.getElementById('myButton').innerHTML="Hit!"; 	
-        return;
-    }
+var myarr = [];
+var average = 0;      
+var xSoundClick = document.getElementById('myAudioClick');        
+var xSoundError = document.getElementById('myAudioError');    
+ 
+function addItem() {
+	//some checks
+	if (myarr.length >= 12) {
+		document.getElementById("myLoader").style.visibility = 'hidden';
+		xSoundError.play()
+		return;
+	};
+	if (!document.getElementById("name").value) {
+		xSoundError.play()
+		return;
+	};
+	if (parseInt(document.getElementById("name").value) < 0 || parseInt(document.getElementById("name").value) > 300){
+		xSoundError.play()
+		return;
+	};
+	//enable loader during the charge of data
+	if (myarr.length < 12) {
+		document.getElementById("myLoader").style.visibility = 'visible';
+	};
+	//save value into the array
+	myarr.push(document.getElementById("name").value);
+	xSoundClick.play()
+	//prints result
+	console.log(myarr.length);
+	if (myarr.length != 12) document.getElementById('itemsLength').innerHTML = myarr.length + 1 +" of 12";
+		else document.getElementById("myLoader").style.visibility = 'hidden';
+	document.getElementById('items').innerHTML = "[" + myarr.join(" , ") + "]";
+	//set focus over textbox
+	document.getElementById("name").focus();
+}
 
-} 
+function infoArray() {
+	//I assume that the first item is the max and the min
+	max=parseInt(myarr[0]);
+	min=parseInt(myarr[0]);
+	//I create an variable to sums items
+	s=0;
+	//I search the max/min values and adds items
+	for (var i = 0; i < myarr.length; i++) {
+		if (parseInt(myarr[i]) > max) max = parseInt(myarr[i]);
+		if (parseInt(myarr[i]) < min) min = parseInt(myarr[i]);
+		s=s+parseInt(myarr[i]);
+	}
+	//calculate the average
+	average=s/i;
+	//check that the array is not empty
+	if(s!=0) document.getElementById('results').innerHTML = "Results: " + "Max: " + max + " || " + "Min: " + min + " || " + "Average: " + average.toFixed(2);
+	myGraph();
+}
 
+function myGraph() {
+	document.getElementById("myLoader").style.visibility = 'hidden';
+	varx=0;
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0, 0, 650, 300);
+	ctx.strokeStyle = '#000000';
+	ctx.fillStyle = '#000000';
+	ctx.font = "15px Arial";
+	ctx.beginPath();
+	for (var i = 0; i < myarr.length; i++) {
+		varx=varx+50;
+		ctx.lineTo(varx, 300-parseInt(myarr[i]));
+		ctx.fillText(myarr[i],varx+5, 300-parseInt(myarr[i]));		
+	}
+	ctx.stroke();
+	//draw the average in red color
+	if(average != 0){
+		ctx.beginPath();
+		ctx.lineWidth = 2;
+		ctx.strokeStyle = '#ff0000';
+		ctx.moveTo(0,300-Math.round(average));
+		ctx.lineTo(650,300-Math.round(average));
+		ctx.fillStyle = '#ff0000';
+		ctx.fillText("AVG:" + Math.round(average),325,300-Math.round(average)-5);
+		ctx.stroke();
+	}
+}
---
+++ js/topic9.js
@@ -0,0 +1,30 @@
+function playAudio() { 
+    var image = document.getElementById('myImage');
+    if (image.src.match("2.png")) {
+        image.src = "../img/3.png";
+        image.style.width = '108px';
+        image.style.height = '147px';
+        var x2 = document.getElementById('myAudio2');        
+        x2.play();    
+        document.getElementById('myButton').innerHTML="Back!";
+        return;
+    }
+    if (image.src.match("1.png")) {
+        image.style.width = '189px';
+        image.style.height = '187px';        
+        image.src = "../img/2.png";
+		var x = document.getElementById('myAudio');
+		document.getElementById('myButton').innerHTML="More!";
+        x.play();
+        return;       	             	
+    }
+    if (image.src.match("3.png")) {
+        image.style.width = '131px';
+        image.style.height = '190px';        
+        image.src = "../img/1.png";  	             
+        document.getElementById('myButton').innerHTML="Hit!"; 	
+        return;
+    }
+
+} 
+
