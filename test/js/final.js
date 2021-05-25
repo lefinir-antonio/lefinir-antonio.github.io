c537375342f1a1fb187bdb70e37704009e9da02d
Antonio Lefinir <alefinir@byui.edu>
2021-05-25 17:30:01 -0300

    add dinamic generation of the cards

--- test/js/final.js
+++ test/js/final.js
@@ -1,9 +1,13 @@
-let jsonparsed;
+var jsonparsed;
+
 function getArticles(){
     req=new XMLHttpRequest();
    req.onreadystatechange=function(){ 
      if (req.readyState === 4 && req.status === 200){ // readyState === 4 significa que está completo y el status === 200 significa que está OK
      jsonparsed = JSON.parse(req.responseText); //Paso 2: Conviertes el JSON en un objeto através de JSON.parse. No te olvides de declarar previamente la variable jsonparsed. 
+
+     printCard();
+
      } else if (req.readyState === 4 && req.status === 404){
        console.log(req.responseText); //Para ver el error en el caso de que no haya podido traerse el JSON
      }
@@ -13,57 +17,57 @@ function getArticles(){
    req.send();
    }
 
-   function myFunction(){
-    getArticles();
-    console.log(jsonparsed[0].h5);
-    console.log(jsonparsed[0].h6);
-    console.log(jsonparsed[0].p);
-    //document.getElementById('myDiv').innerHTML = 'Uruguay Weather';
+   function printCard(){
 
-    // create divcard
-    var divcard = document.createElement('div');
-    //create body of divcard
-    var divcardbody = document.createElement('div');
-    //create h5 and h6
-    var dh5 = document.createElement('h5');
-    var dh6 = document.createElement('h6');
+   // getArticles();
 
-    //create imagen
-    var dimg = document.createElement('img');
-
-    //create paragraph
-    var dp = document.createElement('p');
-
-    //create button
-    var dbutton = document.createElement('button');
-
-    //add json value to h5 and h6
-    dh5.innerHTML = jsonparsed.name;
-    dh6.innerHTML = jsonparsed.country;
-    dimg.src = 'images/promo1.jpg';
-    dp.innerHTML = 'lorem ipsum lorem ipsum lorem ipsum';
-    dbutton.innerHTML='Ver Mas!';
-
-    //change bootstrap classes
-    divcard.className = 'card card-hotel d-flex flex-column justify-content-between ml-2';  
-    divcardbody.className = 'card-body';
-    dh5.className ='card-title';
-    dh6.className ='card-title text-muted';
-    dimg.className = 'card-img-top';
-    dp.className = 'card-text';
-    dbutton.className = 'btn btn-primary btn-reserva';
-  
-    //add h5 and h6 to div
-    divcardbody.appendChild(dh5);
-    divcardbody.appendChild(dh6);
-    divcardbody.appendChild(dimg);
-    divcardbody.appendChild(dp);
-    //add divs to divcard and document
-    divcard.appendChild(divcardbody);
-    divcard.appendChild(dbutton);
-    document.getElementById('myDiv').appendChild(divcard);
+    for(let i = 0; i < jsonparsed.length; i++) {
 
 
-      //div.innerHTML = 'Hi there! ' + jsonparsed.name;
+            // create divcard
+        var divcard = document.createElement('div');
+        //create body of divcard
+        var divcardbody = document.createElement('div');
+        //create h5 and h6
+        var dh5 = document.createElement('h5');
+        var dh6 = document.createElement('h6');
 
-   }
+        //create imagen
+        var dimg = document.createElement('img');
+
+        //create paragraph
+        var dp = document.createElement('p');
+
+        //create button
+        var dbutton = document.createElement('button');
+
+        //add json values to the cards
+        dh5.innerHTML = jsonparsed[i].h5;
+        dh6.innerHTML = jsonparsed[i].h6;
+        dimg.src = jsonparsed[i].img;
+        dp.innerHTML = jsonparsed[i].p;
+        dbutton.innerHTML= jsonparsed[i].button;
+
+        //change bootstrap classes
+        divcard.className = 'card card-hotel d-flex flex-column justify-content-between ml-2';  
+        divcardbody.className = 'card-body';
+        dh5.className ='card-title';
+        dh6.className ='card-title text-muted';
+        dimg.className = 'card-img-top';
+        dp.className = 'card-text';
+        dbutton.className = 'btn btn-primary btn-reserva';
+    
+        //add h5 and h6 to div
+        divcardbody.appendChild(dh5);
+        divcardbody.appendChild(dh6);
+        divcardbody.appendChild(dimg);
+        divcardbody.appendChild(dp);
+        //add divs to divcard and document
+        divcard.appendChild(divcardbody);
+        divcard.appendChild(dbutton);
+        document.getElementById('myDiv').appendChild(divcard);
+        console.log(jsonparsed[i]);  
+    }
+}
+
+   
